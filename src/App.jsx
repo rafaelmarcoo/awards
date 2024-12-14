3ac7731de69bcd856a738302d35912f89726684a
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2024-12-14 20:20:07 +1300

    Setting up colours

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
   return (
     <main>
-      <h1 className="text-5xl text-orange-500 font-bold">Welcome To Awards</h1>
+      <h1 className="text-5xl text-orange-500 font-bold font-circular-web">Welcome To Awards</h1>
     </main>
   );
 }
