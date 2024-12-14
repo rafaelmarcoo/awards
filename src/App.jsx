faa0592bef2f50624132b362b247447130658ade
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2024-12-14 20:03:18 +1300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <main>
+      <h1 className="text-5xl text-orange-500 font-bold">Welcome To Awards</h1>
+    </main>
+  );
+}
+export default App
