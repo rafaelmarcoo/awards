5408761b1224225ef77cb177a1dceae1dea39157
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2024-12-14 20:33:22 +1300

    importes index.css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Hero = () => {
+    const [currentIndex, setCurrentIndex] = useState(1);
+    const [hasClicked, setHasClicked] = useState(false);
+
+    const handleMiniVdClick = () => {
+
+    }
+
     return (
-        <div>
-            
+        <div className="relative h-dvh w-screen overflow-x-hidden">
+            <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-x-hidden rounded-lg bg-blue-75">
+                <div>
+                    <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+                        <div>
+                            MiniVideoPlayer
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
