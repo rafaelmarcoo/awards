bbed538391479f561966adbaf678bc1607731acc
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2024-12-14 20:25:01 +1300

    Component - Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Hero from './components/Hero';
 
 const App = () => {
   return (
-    <main>
-      <h1 className="text-5xl text-orange-500 font-bold font-circular-web">Welcome To Awards</h1>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
     </main>
   );
 }
